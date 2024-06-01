3521f4cc5ea3d3b90e3669e7349f85143a15a44a
brusset.marie <brusset.marie@gmail.com>
2024-06-01 18:34:08 +0200

    passage html-> php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <strong>CONTACTS</strong>
+    <p>MAIL: medicare@omnessante.fr</p>
+    <p>TÉLÉPHONE : 06 45 29 78 11</p>
+    <p>ADRESSE : <a href="https://www.google.com/maps/search/?api=1&query=41+Avenue+de+la+Sant%C3%A9,+PARIS" target="_blank">41 Avenue de la Santé, PARIS</a></p>
+</footer>
+
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php include 'header.php'; ?>
+
+
+<body>
+    <div class="wrapper">
+	<?php include 'bandeau.php'; ?>
+		
+	<main>
+		<div class="texte">
+			<p>Bonjour, bienvenue sur la page d'accueil de Medicare.</p>
+		</div>
+	</main>
+	<?php include 'footer.php'; ?>
+    </div>
+</body>
+</html>
